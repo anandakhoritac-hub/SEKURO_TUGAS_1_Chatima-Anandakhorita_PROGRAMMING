ed2a57f87cd95c6a9fd2c260b0a781a968a39026
anandakhoritac-hub <anandakhoritac@gmail.com>
2026-03-25 15:59:44 +0700

    Add files via upload

---
+++ src/cmake_soal/include/robot.hpp
@@ -0,0 +1,16 @@
+
+#pragma once
+#include "bits/stdc++.h"
+
+class Robot{
+private:
+    int id;
+protected:
+    std::string jenis_robot;
+    int top_speed;
+public :
+   Robot(int id);
+
+   virtual void Komunikasi_Robot();
+   virtual ~Robot();
+};
